78cec7b7139df086959d2295194339bf0770e611
Shahrad Elahi <shahrad@litehex.com>
2025-10-11 10:37:57 +0100

    chore: Update dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
+  clean: true,
+  dts: true,
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  dts: true,
-  splitting: false,
-  sourcemap: true,
-  clean: true,
+  target: 'esnext',
+  outDir: 'dist',
 });
